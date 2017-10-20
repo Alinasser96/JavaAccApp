07597cc9546c75e46970003a9d9f457453ef292f
Alinasser96 <ali96nasser@gmail.com>
2017-10-20 18:10:27 +0200

    origin

--- src/roots/Roots.java
+++ src/roots/Roots.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Roots;
 
 import java.awt.BorderLayout;
@@ -25,7 +21,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -44,26 +42,48 @@ import javax.swing.table.DefaultTableModel;
  */
 public class Roots extends Application {
     double sum=0;
+    double total=0;
     
     @Override
     public void start(Stage primaryStage) {
+        final ToggleGroup group = new ToggleGroup();
         Button btn = new Button();
         Button btn2 = new Button();
         Button btn3 = new Button();
         Button btn4 = new Button();
         Button btn5 = new Button();
+        Button btn6 = new Button();
+        RadioButton rb1 = new RadioButton("عميل");
+        rb1.setToggleGroup(group);
+        rb1.setSelected(true);
+
+        RadioButton rb2 = new RadioButton("تاريخ");
+        rb2.setToggleGroup(group);
+
+        RadioButton rb3 = new RadioButton("رقم فاتوره");
+        rb3.setToggleGroup(group);
+        
+        RadioButton rb4 = new RadioButton("كشف حساب");
+        
+
+        rb1.setUserData("client");
+        rb2.setUserData("date");
+        rb3.setUserData("fnum");
+        rb3.setUserData("prie");
+        
+    
         TextField namef = new TextField();
         TextField names = new TextField();
         TextField phonef  = new TextField();
         TextField emailf = new TextField();
         TextField noo3f = new TextField();
-        TextField fe2af = new TextField();
-        TextField numf  = new TextField();
+        
         
         Label namel = new Label("اسم العميل ");
-        Label namels = new Label("بحث باسم العميل ");
+        Label namels = new Label("بحث عن ");
         Label phonel = new Label("هاتف");
         Label emaill = new Label("ايميل");
+        Label hsap = new Label("إجمالي");
         Label datel = new Label("تاريخ الفاتورة");
         Label knuml = new Label("دفتر رقم");
         Label fnuml = new Label("فاتورة رقم");
@@ -115,15 +135,32 @@ public class Roots extends Application {
             
             @Override
             public void handle(ActionEvent event) {
+                total=0;
                  Connection conn = null;
                  Statement st = null;
                  ResultSet rs = null;
                  String s ; 
+                 String keyword="" ;
                  String name= names.getText();
+                            if(rb1.isSelected()){
+                   keyword= "client";
+                   }
+                   if(rb2.isSelected()){
+                   keyword= "date";
+                   }
+                   if(rb3.isSelected()){
+                   keyword= "fnum";
+                   }
+                   if(rb4.isSelected()){
+                   noo3f.setVisible(true);
+                   }
+                   else{
+                   noo3f.setVisible(false);
+                   }
                  try {
                      conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/rootsAcc","root","");
                      st=conn.createStatement();
-                     s="select * from data WHERE client LIKE '%"+name+"%'";
+                     s="select * from data WHERE "+keyword+" LIKE '%"+name+"%'";
                      rs = st.executeQuery(s);
                      ResultSetMetaData rsmt = rs.getMetaData();
                      int c = rsmt.getColumnCount();
@@ -141,12 +178,16 @@ public class Roots extends Application {
                      row = new Vector(c);
                      for (int i =1 ; i<=c ;i++){
                      row.add(rs.getString(i));
+                     if(i==6){
+                     total+=Double.parseDouble(rs.getString(i));
+                     }
                      
                      }
                      data.add(row);
                      
                      
                      }
+                     noo3f.setText(""+total+"");
                      
                      
                      
@@ -157,7 +198,7 @@ public class Roots extends Application {
                      
                      JPanel panel = new JPanel();
                      JTable table = new JTable(data,coulmn);
-                     int k = table.getSelectedRow();
+                     
                      
                     
                      JScrollPane jsp = new JScrollPane(table);
@@ -183,7 +224,7 @@ public class Roots extends Application {
         
         
         
-        btn3.setText("+");
+        btn3.setText("فاتورة جديدة");
         btn3.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
@@ -217,8 +258,8 @@ public class Roots extends Application {
                      knumf.setBounds(500, 280, 100, 25);
                      fnumf.setBounds(500, 310, 100, 25);
 
-                     JButton btnAdd = new JButton("Add");
-                     JButton btnAddsql = new JButton("Addsql");
+                     JButton btnAdd = new JButton("إضافة عنصر");
+                     JButton btnAddsql = new JButton("إضافة الفاتورة");
                      btnAdd.setBounds(150, 220, 100, 25);
                      btnAddsql.setBounds(630, 220, 100, 25);
                      JScrollPane pane = new JScrollPane(table);
@@ -302,26 +343,119 @@ public class Roots extends Application {
             }
         });
         
+        btn4.setText("استلام نقدية");
+        btn4.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                
+                JFrame frame = new JFrame();
+                     frame.setSize(500,120);
+                     frame.setLocationRelativeTo(null);
+                     
+                     
+                     
+                     
+                    
+                     JTextField facnamef = new JTextField();
+                     JTextField datef = new JTextField();
+                     JTextField knumf = new JTextField();
+                     
+                     
+                     
+                     facnamef.setBounds(500, 220, 100, 25);
+                     datef.setBounds(500, 250, 100, 25);
+                     knumf.setBounds(500, 280, 100, 25);
+                     
+
+                     
+                     JButton btnAddsql = new JButton("إضافة");
+                     
+                     btnAddsql.setBounds(630, 220, 100, 25);
+                     
+                     
+        
+                     frame.setLayout(null);
+        
+                     
+                     frame.add(facnamef);
+                     frame.add(datef);
+                     frame.add(knumf);
+                     
+                     frame.add(btnAddsql);
+                     
+                     
+                   
+                     
+                     
+                // button add row
+  
+        btnAddsql.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e ) {
+             
+                
+                String facname= facnamef.getText();
+                String date= datef.getText();
+                String knum= knumf.getText();
+                double xxx= Double.parseDouble(knum);
+                
+                
+                Connection conn;
+                try {
+                    conn = getmysqlconnection();
+                    insertData(conn,facname,date,knum,"",-1*xxx,"","","");
+                    JOptionPane.showMessageDialog(null, "تمت الاضافة");
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "حدث خطأ ");
+                }
+                facnamef.setText("");
+                datef.setText("");
+                knumf.setText("");
+                
+                
+                
+               
+                
+            }
+        });
+        
+                       frame.setSize(900,400);
+        frame.setLocationRelativeTo(null);
+        
+        frame.setVisible(true);
+             
+            }
+        });
+        
         
         
         GridPane root = new GridPane();
+        root.add(rb1 ,4,0);
+        root.add(rb2 ,5,0);
+        root.add(rb3 ,6,0);
+        root.add(rb4 ,6,1);
         root.add(namef ,0,0);
-        root.add(names ,2,0);
+        root.add(names ,5,2);
         root.add(phonef,0,1);
         root.add(emailf,0,2);
         root.add(namel, 1, 0);
-        root.add(namels, 3, 0);
+        root.add(namels, 6, 2);
+        root.add(hsap, 6, 3);
         root.add(phonel,1,1);
         root.add(emaill,1,2);
-        root.add(btn,2,2);
+        root.add(btn,0,4);
         root.add(btn2,4,2);
-        root.add(btn3,5,2);
+        root.add(btn3,3,4);
+        root.add(btn4,4,4);
+        root.add(noo3f,5,3);
         root.setAlignment(Pos.CENTER);
         root.setHgap(20);
         root.setVgap(20);
         root.setPadding(new Insets(25,25,25,25));
         
-        Scene scene = new Scene(root, 600, 600);
+        Scene scene = new Scene(root, 800, 400);
        
         
         
@@ -336,27 +470,20 @@ public class Roots extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
-    
-    
-    
-     public static ResultSet searchclient(Connection conn,String name)throws Exception{
+    public static ResultSet searchclient(Connection conn,String name)throws Exception{
        
     return getresultset(conn,"select * from client where name like '%"+name+"%'");
     }
-     public static ResultSet searchfat(Connection conn,String name)throws Exception{
+    public static ResultSet searchfat(Connection conn,String name)throws Exception{
        
     return getresultset(conn,"select * from data where clint like '%"+name+"%'");
     }
-
     public static ResultSet getresultset (Connection conn ,String sql)throws Exception{
     Class.forName("com.mysql.jdbc.Driver");
     Statement  stm = conn.createStatement(java.sql.ResultSet.CONCUR_READ_ONLY,java.sql.ResultSet.TYPE_FORWARD_ONLY);
     return stm.executeQuery(sql);
     
     }
-
-
     public static void insertClient(Connection conn ,String name,String phone , String email) throws Exception{
 
 
@@ -386,9 +513,6 @@ public class Roots extends Application {
 
 
     }
-
-
-
     public static Connection getmysqlconnection() throws Exception{
     Class.forName("com.mysql.jdbc.Driver");
     return DriverManager.getConnection("jdbc:mysql://127.0.0.1/rootsAcc","root","");
